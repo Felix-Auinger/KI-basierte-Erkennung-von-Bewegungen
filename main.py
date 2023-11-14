6e5484ea5765ef5d92a1081a223955f4e12792d9
TheBillyTheHeide <billyheide26@gmail.com>
2023-11-14 20:13:30 +0100

    dual camera integration
    dual camera integration

--- main.py
+++ main.py
@@ -1,43 +1,64 @@
+import threading
 import cv2
 from ultralytics import YOLO
 
-def main():
+
+def run_tracker_in_thread(filename, model, file_index):
     """
-    Primary Prototype of Webcam Based YoloV8 Pose Detection
+    Runs a video file or webcam stream concurrently with the YOLOv8 model using threading.
+
+    This function captures video frames from a given file or camera source and utilizes the YOLOv8 model for object
+    tracking. The function runs in its own thread for concurrent processing.
+
+    Args:
+        filename (str): The path to the video file or the identifier for the webcam/external camera source.
+        model (obj): The YOLOv8 model object.
+        file_index (int): An index to uniquely identify the file being processed, used for display purposes.
+
+    Note:
+        Press 'q' to quit the video display window.
     """
+    video = cv2.VideoCapture(filename)  # Read the video file
+
+    while True:
+        ret, frame = video.read()  # Read the video frames
+
+        # Exit the loop if no more frames in either video
+        if not ret:
+            break
+
+        # Track objects in frames if available
+        results = model.track(frame, persist=True)
+        res_plotted = results[0].plot()
+        cv2.imshow(f"Tracking_Stream_{file_index}", res_plotted)
+
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+
+    # Release video sources
+    video.release()
 
 
-# Load the YOLOv8 model
-    model = YOLO('yolov8n-pose.pt')
+# Load the models
+model1 = YOLO('yolov8n-pose.pt')
+model2 = YOLO('yolov8n-pose.pt')
 
-    # Open camera
-    cap = cv2.VideoCapture(0)
+# Define the video files for the trackers
+video_file1 = "test.mp4"  # Path to video file, 0 for webcam
+video_file2 = 0  # Path to video file, 0 for webcam, 1 for external camera
 
-    # Loop through the video frames
-    while cap.isOpened():
-        # Read a frame from the video
-        success, frame = cap.read()
+# Create the tracker threads
+tracker_thread1 = threading.Thread(target=run_tracker_in_thread, args=(video_file1, model1, 1), daemon=True)
+tracker_thread2 = threading.Thread(target=run_tracker_in_thread, args=(video_file2, model2, 2), daemon=True)
 
-        if success:
-            # Run YOLOv8 tracking on the frame, persisting tracks between frames
-            results = model.track(frame, persist=True)
+# Start the tracker threads
+tracker_thread1.start()
+tracker_thread2.start()
 
-            # Visualize the results on the frame
-            annotated_frame = results[0].plot()
+# Wait for the tracker threads to finish
+tracker_thread1.join()
+tracker_thread2.join()
 
-            # Display the annotated frame
-            cv2.imshow("YOLOv8 Tracking", annotated_frame)
-
-            # Break the loop if 'q' is pressed
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                break
-        else:
-            print("Error in Live Detection or Webcam")
-
-    # Release the video capture object and close the display window
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-if __name__ == "__main__":
-    main()
+# Clean up and close windows
+cv2.destroyAllWindows()
